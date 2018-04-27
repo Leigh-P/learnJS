17de537a37a03bd9a9acad38760d2a23bf919024
jingleiliu <911982917@qq.com>
2018-04-28 00:35:11 +0800

    add img to be shown

--- demo/banner/data/data.js
+++ demo/banner/data/data.js
@@ -1,5 +1,18 @@
 data = [
     {
-        
+        backColor: "#d3d3d3",
+        src: "../images/cat_1.jpeg"
+    },
+    {
+        backColor: "#d3d3d3",
+        src: "../images/cat_2.jpeg"
+    },
+    {
+        backColor: "#ff8dc",
+        src: "../images/cat_3.jpeg"
+    },
+    {
+        backColor: "#ffffff",
+        src: "../images/cat_4.jpeg"
     }
 ] 
