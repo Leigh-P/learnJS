b80eb227a065c8779d798086e4684f3cfaad6ec0
jingleiliu <911982917@qq.com>
2018-04-28 13:51:23 +0800

    <feat>:<class Image method of autoPlay>

--- demo/banner/scripts/image.js
+++ demo/banner/scripts/image.js
@@ -8,12 +8,28 @@ class Image {
     init() {
         this.btns[0].className = "circle on";
         this.imgBox.src = this.data[0].src;
+        this.autoPlay(0);
     }
     clickPlay() {
-        
+
     }
     autoPlay() {
-
+        let that = this;
+        function Play(index) {
+            that.imgBox.src = that.data[index].src;
+            that.btns[index].className = "circle on"
+            if (index == 0) {
+                that.btns[3].className = "circle";
+            } else {
+                that.btns[index - 1].className = "circle";
+            }
+        } 
+        let num = 0;
+        setInterval(function () {
+            Play(num);
+            num++;
+            if (num > 3) num = 0;
+        }, 3000)
     }
 }
 let imgae = new Image(data, 'button', 'img', 'circle');
